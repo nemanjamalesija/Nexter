86cc43864cf91457956dfa571ee3af97a5dc7e14
nemanjamalesija <malesija13@gmail.com>
2023-04-17 16:54:53 +0200

    Set 2D layout for the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 function App() {
-  return <div className='app'>Slobodan</div>;
+  return (
+    <div className='container'>
+      <div className='sidebar'>Sidebar</div>
+      <header className='header'>Header</header>
+      <div className='realtors'>Top 3 realtors</div>
+      <section className='features'>Features</section>
+      <div className='story__pictures'>Story pictures</div>
+      <div className='story__content'>Story content</div>
+      <section className='homes'>Homes</section>
+      <section className='gallery'>Gallery</section>
+      <footer className='footer'>Footer</footer>
+    </div>
+  );
 }
 
 export default App;
